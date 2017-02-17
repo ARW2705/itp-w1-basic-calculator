cf2cd5196cc203a61458817020b9f7ac325aaaae
Andrew Wanex <awanex2705@gmail.com>
2017-02-17 00:46:31 +0000

    Initial commit

--- basic_calculator/main.py
+++ basic_calculator/main.py
@@ -1,5 +1,17 @@
 """This is the entry point of the program."""
 
 
-def basic_calculator(num1, num2, operation):
-    pass
+def basic_calculator(num_1, num_2, oper):
+    if oper == 'add':
+        return num_1 + num_2
+    elif oper == 'subtract':
+        return num_1 - num_2
+    elif oper == 'multiply':
+        return num_1 * num_2
+    elif oper == 'divide':
+        if num_2 == 0:
+            return 'Does not exist'
+        else:
+            return num_1 / num_2
+    else:
+        return 'Invalid operation'
